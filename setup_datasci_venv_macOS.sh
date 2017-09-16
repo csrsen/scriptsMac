62a3e80a989f342b78c1eb5d3e865124a71e131c
Craig Srsen <csrsen@gmail.com>
2017-09-15 21:41:33 -0700

    moved Julia install to Caskfile

---
+++ setup_datasci_venv_macOS.sh
@@ -0,0 +1,14 @@
+# Python virtual environment setup script
+# you can just drag this file into a Terminal.app window to execute
+
+#!/bin/bash
+
+# create a new virtual environment
+pyvenv datasci
+source datasci/bin/activate
+
+# install packages
+pip3 install pandas sqlalchemy ipython
+
+
+source deactivate
