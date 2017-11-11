0bea3ce5cb9f865df76cddbd7450432634b24ece
Craig Srsen <csrsen@gmail.com>
2017-11-10 16:28:56 -0800

    added Powershell language

--- APM_install.sh
+++ APM_install.sh
@@ -2,6 +2,6 @@
 
 # Atom must be installed
 
-apm install dash minimap language-coffee-script language-applescript
+apm install dash minimap language-coffee-script language-applescript language-powershell
 
 echo "Script $(basename \"$0\") complete."
