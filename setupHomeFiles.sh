1bd955592751f2a238d3c13e86bdf603d2cbfcdc
Craig Srsen <csrsen@gmail.com>
2018-02-07 14:15:21 -0800

    initial commit

---
+++ setupHomeFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Setup script for new Mac
+
+cd ~
+wget https://raw.githubusercontent.com/csrsen/scriptsMac/master/.brew
+wget https://raw.githubusercontent.com/csrsen/scriptsMac/master/Brewfile
+wget https://raw.githubusercontent.com/csrsen/scriptsMac/master/Caskfile
+wget https://raw.githubusercontent.com/csrsen/scriptsMac/master/new_install_macOS.sh
